a58d5df7fd0f4cd8ae2b95a41a3c1ba6647316f4
Rodrigo Vargas <rodrigo.vargas@conectt.com.br>
2019-07-14 16:07:59 -0300

    Added browser sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
+const browsersync    = require("browser-sync").create();
 const gulp           = require("gulp");
 const plumber        = require("gulp-plumber");
 const sass           = require("gulp-sass");
+
+function browserSync(done) {
+    browsersync.init({
+       proxy: "uphealth.test"
+    });
+    done();
+ }
 
 function css() {
     return gulp
@@ -8,13 +16,14 @@ function css() {
        .pipe(plumber())
        .pipe(sass({ outputStyle: "expanded" }))
        .on("error", sass.logError)
-       .pipe(gulp.dest("./"));
+       .pipe(gulp.dest("./"))
+       .pipe(browsersync.stream());
 }
 
 function watchFiles() {
     gulp.watch("src/scss/**/*.scss", css);
 } 
 
-const start = gulp.parallel(css, watchFiles);
+const start = gulp.parallel(css, watchFiles, browserSync);
 
 exports.default = start;
